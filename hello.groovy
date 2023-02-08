0c555079f1efe38ad29f74912f236e067ff8a1bc
j.richard <j.richard@technologyandstrategy.com>
2023-02-08 15:34:32 +0100

    :bug:

--- hello.groovy
+++ hello.groovy
@@ -1,9 +1,9 @@
 def example1() {
-  println 'Hello from example1 function !'
+  println 'Hello from example1 function !\n'
 }
 
 def example2() {
-  println 'Hello from example2 function'
+  println 'Hello from example2 function\n'
   def proc = 'bash hello.sh'.execute()
   def b = new StringBuffer()
   println proc.text
